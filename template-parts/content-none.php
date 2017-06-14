b59c89bd3d1d208c7ec74cae06d3e112324e6aa8
Tom Myruski <tomhavok@gmail.com>
2017-06-14 18:19:27 -0400

    Todays work

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -9,7 +9,7 @@
 
 ?>
 
-<section class="no-results not-found">
+<section class="no-results not-found"><a href="/wp-content/themes/allmobilevideo-theme/test.html">Proceed to Test</a>
 	<header class="page-header">
 		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'allmobilevideo-theme' ); ?></h1>
 	</header><!-- .page-header -->
